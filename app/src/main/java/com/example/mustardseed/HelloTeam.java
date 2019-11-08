33ee2f81c9ebb8b116667dfb59aa63804eb9d63b
Murray, Ashley <heh18001@byui.edu>
2019-11-07 20:50:22 -0700

    updating with the Test Files and imports

--- app/src/main/java/com/example/mustardseed/HelloTeam.java
+++ app/src/main/java/com/example/mustardseed/HelloTeam.java
@@ -3,5 +3,4 @@ package com.example.mustardseed;
 public class HelloTeam {
     public String conflict = "CONFLICT!!!";
     public String team;
-    System.out.println("Hello Teammates!");
 }
---
+++ app/src/test/java/com/example/mustardseed/testStreak.java
@@ -0,0 +1,19 @@
+package com.example.mustardseed;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testStreak {
+    @Test
+    public void testIncreaseStreak(){
+//        Streak currentStreak.setCurrentStreak(1);
+//
+//        currentStreak.updateStreak(1);
+//
+//        assertEquals(2,currentStreak.getCurrentStreak)
+     assertEquals(true,compareStreak(1,1));
+
+    }
+
+
+}
