c3f47ebc641a304f30700b3a3dc9605a41ec6499
TheJacobJohnson <jacob.w.johnson12@icloud.com>
2019-11-12 21:25:02 -0700

    Added saveUser and loadUser for sharedPref in ProfileActivity (User) Only works for fullName

--- app/src/main/java/com/example/mustardseed/ProfileActivity.java
+++ app/src/main/java/com/example/mustardseed/ProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.mustardseed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        loadUser();
+    }
+
+    String _fullName;
+
+    //Saves data to sharedPreferences
+    private void saveUser(){
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("savedUser",0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        EditText uName = findViewById(R.id.etFullName);
+        String sName = uName.toString();
+        editor.putString("FullName" ,sName);
+        editor.commit();
+    }
+
+    private void loadUser() {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("savedUser",0);
+        _fullName = sharedPref.getString("FullName", "");
+        EditText fullName = (EditText) findViewById(R.id.etFullName);
+        fullName.setText(_fullName);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.saveUser();
     }
 }
