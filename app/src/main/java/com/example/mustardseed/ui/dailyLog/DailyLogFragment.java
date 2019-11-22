fc1546a069e4ff88ab513615dfb324977ff56d22
TheJacobJohnson <jacob.w.johnson12@icloud.com>
2019-11-22 13:51:32 -0700

    Finished sharedPref for dailyLog. Now saves and loads completed days correctly. Also changed minAPI in build.gradle to 26 from 24.

--- app/src/main/java/com/example/mustardseed/ui/dailyLog/DailyLogFragment.java
+++ app/src/main/java/com/example/mustardseed/ui/dailyLog/DailyLogFragment.java
@@ -21,9 +21,18 @@ import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.example.mustardseed.DailyLogLogic;
 import com.example.mustardseed.R;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class DailyLogFragment extends Fragment {
@@ -31,9 +40,11 @@ public class DailyLogFragment extends Fragment {
     private DailyLogViewModel dailyLogViewModel;
     private CalendarView _CalendarView;
     private Switch _readingSwitch;
-    private List<EventDay> _loggedDays = new ArrayList<>();
+    private List<EventDay> _savedEventDays = new ArrayList<>();
     private List<Calendar> _daysRead = new ArrayList<>();
-    private Calendar currDay;
+    private Calendar currCal;
+    private LocalDate currDate;
+    private List<LocalDate> _savedDates = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -47,13 +58,26 @@ public class DailyLogFragment extends Fragment {
 
         //Load sharedPref
         SharedPreferences preferences = this.getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        String gDaysRead = preferences.getString("dailyLogDays", null);
-        Log.i("DailyLogFragment", "Read Preference = " + gDaysRead);
-        if (gDaysRead != null) {
-            Gson gson = new Gson();
-            _daysRead = (List<Calendar>) gson.fromJson(gDaysRead, List.class);
-            Log.i("DailyLogFragment", "_daysRead after gson = " + _daysRead);
+//        preferences.edit().clear();
+        String gDateString = preferences.getString("dailyLogDateArray", null);
+
+        Gson gson = new Gson();
+        String[] dateStrings = gson.fromJson(gDateString, String[].class);
+
+        for (int i = 0; i<dateStrings.length; i++){
+            LocalDate ld = LocalDate.parse(dateStrings[i]);
+            Date date = Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            _daysRead.add(cal);
+            _savedDates.add(ld);
         }
+
+        Log.i("DailyLogFragment", "_daysRead = "+ _daysRead);
+        Log.i("DailyLogFragment", "_savedDates = "+ _savedDates);
+
+        //Make sure set days are Highlighted.
+        _CalendarView.setHighlightedDays(_daysRead);
 
         // setup Calendar click listener
         _CalendarView.setOnDayClickListener(new OnDayClickListener() {
@@ -62,25 +86,37 @@ public class DailyLogFragment extends Fragment {
 //                Log.i("DailyLogFragment", "Event Day clicked is: " + eventDay);
 //                Log.i("DailyLogFragment", "Before clicked _daysRead has: " + _daysRead.size());
 //                Log.i("DailyLogFragment", "Before clicked currDay is: " + currDay);
-                currDay = eventDay.getCalendar();
-                _readingSwitch.setChecked(_daysRead.contains(currDay));
+                currCal = eventDay.getCalendar();
+                currDate = currCal.getTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                _readingSwitch.setChecked(_savedDates.contains(currDate));
 //                Log.i("DailyLogFragment", "After click event Day clicked is: " + eventDay);
 //                Log.i("DailyLogFragment", "After clicked _daysRead has: " + _daysRead.size());
-//                Log.i("DailyLogFragment", "After clicked currDay is: " + currDay);
+                Log.i("DailyLogFragment", "After clicked currDay is: " + currDate);
+                Log.i("DailyLogFragment", "_daysRead = " + _daysRead);
+                Log.i("DailyLogFragment", "_savedDates = " + _savedDates);
             }
         });
 
         // setup toggle for listener
         _readingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Calendar selectedDate = currDay;
-                System.out.println(currDay);
+                Calendar selectedCal = currCal;
+                LocalDate selectedDate = currDate;
+                System.out.println(currDate);
                 if (isChecked) {
-                    _daysRead.add(selectedDate);
+                    if(_savedDates.contains(currDate)){
+
+                    }else {
+                        _daysRead.add(selectedCal);
+                        _savedDates.add(selectedDate);
+                    }
                 }
                 else {
-                   _daysRead.remove(selectedDate);
+                   _daysRead.remove(selectedCal);
+                   _savedDates.remove(selectedDate);
                 }
+                Log.i("DailyLogFragment", "_dailyLog after toggle = " + _daysRead);
+                Log.i("DailyLogFragment", "_savedDates after toggle = " + _savedDates);
                 _CalendarView.setHighlightedDays(_daysRead);
             }
         });
@@ -92,9 +128,14 @@ public class DailyLogFragment extends Fragment {
         SharedPreferences.Editor editor = this.getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE).edit();
         Gson gson = new Gson();
 
-        String daysRead = gson.toJson(_daysRead);
-        Log.i("DailyLogFragment", "Converted Gson = " + daysRead);
-        editor.putString("dailyLogDays", daysRead);
+        //Change each LocalDate in the savedDates to a String[] then serialize and save
+        String[] dateStrings = new String[_savedDates.size()];
+        for (int i = 0; i< _savedDates.size(); i++){
+            dateStrings[i] = _savedDates.get(i).toString();
+        }
+        String gDateString = gson.toJson(dateStrings);
+        Log.i("DailyLogFragment", "String[] dateStrings in JSON =" + gDateString);
+        editor.putString("dailyLogDateArray", gDateString);
         editor.commit();
     }
 }
