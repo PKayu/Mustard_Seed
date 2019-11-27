75e72e09b861e3668bed597c2ad5020817c8f898
Murray, Ashley <heh18001@byui.edu>
2019-11-26 23:07:19 -0700

    Updated Goal Screen with Titles and only allows user to select 1-7 for number of days.

--- app/src/main/java/com/example/mustardseed/ui/goal/GoalFragment.java
+++ app/src/main/java/com/example/mustardseed/ui/goal/GoalFragment.java
@@ -1,28 +1,37 @@
 package com.example.mustardseed.ui.goal;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.mustardseed.Goal;
+import com.example.mustardseed.Notification;
 import com.example.mustardseed.StartDatePickerFragment;
 import com.example.mustardseed.EndDatePickerFragment;
 import com.example.mustardseed.MainActivity;
 import com.example.mustardseed.R;
-import com.example.mustardseed.StartDatePickerFragment;
+import com.example.mustardseed.User;
+import com.google.gson.Gson;
 
 import org.w3c.dom.Text;
 
@@ -39,6 +48,10 @@ public class GoalFragment extends Fragment {
     private EditText mStartDisplayDate;
     private EditText mEndDisplayDate;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private Button _setGoal;
+    private EditText _startDate;
+    private EditText _endDate;
+    private EditText _NumDaysWeek;
 
 
     private GoalViewModel goalViewModel;
@@ -56,12 +69,40 @@ public class GoalFragment extends Fragment {
                 ViewModelProviders.of(this).get(GoalViewModel.class);
         View root = inflater.inflate(R.layout.fragment_goal, container, false);
 
-        //allows for the Date selector ( Will come back and work on this later)
+        //allows for the Date selector
         mStartDisplayDate = root.findViewById(R.id.startDate);
         mEndDisplayDate = root.findViewById(R.id.endDate);
 
         mStartDisplayDate.setOnClickListener(this::showStartDatePickerDialog);
         mEndDisplayDate.setOnClickListener(this::showEndDatePickerDialog);
+
+        //Get UI for manipulation
+        _setGoal = root.findViewById(R.id.setGoal);
+        _startDate = root.findViewById(R.id.startDate);
+        _endDate = root.findViewById(R.id.endDate);
+        _NumDaysWeek = root.findViewById(R.id.numDays);
+
+        //only allow 1-7 in the num days section
+        _NumDaysWeek.setFilters(new InputFilter[] { new InputFilterMinMax("1", "7")});
+
+        // setup save btn click listener
+        Log.i(TAG, "Create listener");
+        _setGoal.setOnClickListener(this::onSaveClick);
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("goal", Context.MODE_PRIVATE);
+        String gGoal = preferences.getString("goal", null);
+
+        Gson gson = new Gson();
+        Goal goal = gson.fromJson(gGoal,Goal.class);
+
+        if(goal != null) {
+            Log.i(TAG, "Load stored preferences");
+            _startDate.setText(goal.getStartGoal());
+            _endDate.setText(goal.getEndGoal());
+            _NumDaysWeek.setText(goal.getNumDays());
+            Log.i(TAG, "Finish loading stored preferences");
+        }
+
 
         return root;
     }
@@ -80,4 +121,24 @@ public class GoalFragment extends Fragment {
         DialogFragment newFragment = new EndDatePickerFragment();
         newFragment.show(this.getActivity().getSupportFragmentManager(), "endPicker");
     }
-}
+
+    public void onSaveClick(View view){
+        Log.i("GoalFragment", "Save clicked");
+
+        String sNumDays = _NumDaysWeek.getText().toString();
+        String sStartDate = _startDate.getText().toString();
+        String sEndDate = _endDate.getText().toString();
+
+        Goal goal = new Goal(sNumDays, sStartDate, sEndDate);
+        Gson gson = new Gson();
+
+        String gGoal = gson.toJson(goal);
+
+        SharedPreferences sharedPref = view.getContext().getSharedPreferences("goal", Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = sharedPref.edit();
+        prefEditor.putString("goal", gGoal);
+        prefEditor.commit();
+        Toast.makeText(view.getContext().getApplicationContext(),"Your goal has been saved successfully", Toast.LENGTH_LONG).show();
+    }
+
+};
