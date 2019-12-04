72b7f3892d0b00968e720021d3032e7c5f292111
Murray, Ashley <heh18001@byui.edu>
2019-12-03 21:00:42 -0700

    Load a Null goal correctly on all screens.

---
+++ app/src/test/java/com/example/mustardseed/ui/goal/GoalFragmentTest.java
@@ -0,0 +1,22 @@
+package com.example.mustardseed.ui.goal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoalFragmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void showEndDatePickerDialog() {
+    }
+}
